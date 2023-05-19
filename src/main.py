33a0b616cd4c94c1c3aa6c04fb13c388bb314aa4
GiselleP <pachecog2706@gmail.com>
2023-05-19 17:45:27 -0600

    saving imgs of regions selected

--- src/main.py
+++ src/main.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 
 video = cv2.VideoCapture('/home/giselle/Documents/UDEM/Computer-vision/Codigos/tercer-parcial/2023_05_05_14_59_37-ball-detection.mp4')
 
-
 # Global variables
 drawing = False
 points = []
+mask = None
 
 # Mouse callback function
 def mouse_callback(event, x, y, flags, param):
-    global drawing, points
+    global drawing, points, mask
 
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
@@ -24,7 +24,11 @@ def mouse_callback(event, x, y, flags, param):
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
 
-# Load the video
+        # Create a mask based on the contour
+        mask = np.zeros(frame.shape[:2], np.uint8)
+        pts = np.array(points, np.int32)
+        cv2.fillPoly(mask, [pts], 255)
+
 
 # Create a window and set the mouse callback function
 cv2.namedWindow('Video')
@@ -41,20 +45,29 @@ while video.isOpened():
     for point in points:
         cv2.circle(frame, point, 2, (0, 255, 0), -1)
 
-    # Convert points to a NumPy array
-    pts = np.array(points, np.int32)
-    pts = pts.reshape((-1, 1, 2))
+    # Create a copy of the frame
+    frame_copy = frame.copy()
 
-    # Draw the contour if at least 3 points are selected
-    if len(pts) >= 3:
-        cv2.drawContours(frame, [pts], -1, (0, 255, 0), 2)
+    # Apply the mask to the frame
+    if mask is not None:
+        frame_copy = cv2.bitwise_and(frame_copy, frame_copy, mask=mask)
 
     # Display the resulting frame
-    cv2.imshow('Video', frame)
+    cv2.imshow('Video', frame_copy)
+
+    # Save the selected region to a separate image
+    if mask is not None:
+        selected_region = cv2.bitwise_and(frame, frame, mask=mask)
+        cv2.imwrite('selected_region.png', selected_region)
+        print("Selected region saved as 'selected_region.png'")
+
+        # Reset the mask and points
+        mask = None
+        points = []
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Release the video capture and close the window
 video.release()
 cv2.destroyAllWindows()
-
