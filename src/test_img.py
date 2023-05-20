9ae7636b63a201a16744f5e567f5b6d2c7ec9c28
Juan Carlos <juan.sanchezp@udem.edu>
2023-05-20 12:06:22 -0600

    plot blue channel histogram of neighborhood

--- src/test_img.py
+++ src/test_img.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import os
 import datetime
+import matplotlib.pyplot as plt
 
 # Global variables
 clicked_points = []
@@ -18,8 +19,33 @@ def crop_image(image, points):
     mask = np.zeros(image.shape[:2], dtype=np.uint8)
     points = np.array(points)
     cv2.fillPoly(mask, [points], (255, 255, 255))
+    neighborhood_size = 5
+    dilated_mask = cv2.dilate(mask, np.ones((neighborhood_size, neighborhood_size), np.uint8))
+    neighborhood = cv2.absdiff(dilated_mask, mask)
+    neighborhood_pixels = np.where(neighborhood > 0)
+
+    neighborhood_region = image[min(neighborhood_pixels[0]):max(neighborhood_pixels[0]) + 1,
+                            min(neighborhood_pixels[1]):max(neighborhood_pixels[1]) + 1]
+
+    # Generate the histogram of the neighborhood
+    histB = cv2.calcHist([neighborhood_region], [0], None, [256], [0, 256])
+
+    # Plot the histogram
+    plt.plot(histB,color='b')
+    plt.title("Histogram of the Neighborhood")
+    plt.xlabel("Pixel Value")
+    plt.ylabel("Frequency")
+    plt.legend('B')
+    plt.show()
+
+    # Visualize the neighborhood
+    image[neighborhood_pixels] = [0, 0, 255]  # Mark neighborhood pixels as red
+
+    cv2.imshow("Image with Neighborhood", image)
+
+    #cv2.imshow('dilated_mask',neighborhood)
     cropped_image = cv2.bitwise_and(image, image, mask=mask)
-    return cropped_image
+    return cropped_image,mask
 
 # Read video file
 video_path = "/home/enchi/Vídeos/2023_05_05_14_59_37-ball-detection.mp4"  # Replace with your video file path"
@@ -44,7 +70,7 @@ while cap.isOpened():
         break
 
     if crop_mode and len(clicked_points) >= 3:
-        cropped_frame = crop_image(frame, clicked_points)
+        cropped_frame,mask = crop_image(frame, clicked_points)
         cv2.imshow(window_name, cropped_frame)
     else:
         # Draw clicked points
@@ -73,13 +99,12 @@ while cap.isOpened():
             timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
             save_directory = os.path.abspath(os.path.join(os.getcwd(), "..", "img"))  # Replace "desired_directory" with your desired directory name
             os.makedirs(save_directory, exist_ok=True)
-            save_path = os.path.join(save_directory, "cropped_image_{}.jpg".format(timestamp))
-            cv2.imwrite(save_path, cropped_frame)
+            save_path = os.path.join(save_directory, "mask_{}.jpg".format(timestamp))
+            cv2.imwrite(save_path,mask)
             print("Cropped image saved as:", save_path)
         else:
             print("Cannot save cropped image. Please enter crop mode and select at least 3 points.")
-
-       
+   
 
     # Exit if 'q' is pressed
     if key == ord('q'):
