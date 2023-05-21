61316672d5ce2c5a82c69199507d598358fc0950
Juan Carlos <juan.sanchezp@udem.edu>
2023-05-21 16:44:47 -0600

    codigo para poder tomar parches de el primer frame de nuestro video

--- src/test_img.py
+++ src/test_img.py
@@ -23,9 +23,7 @@ def crop_image(image, points):
     neighborhood_size = 5
     dilated_mask = cv2.dilate(mask, np.ones((neighborhood_size, neighborhood_size), np.uint8))
     neighborhood = cv2.absdiff(dilated_mask, mask)
-    #neighborhood_pixels = np.where(neighborhood > 0)
-    #neighborhood_region = image[min(neighborhood_pixels[0]):max(neighborhood_pixels[0]) + 1,
-     #                      min(neighborhood_pixels[1]):max(neighborhood_pixels[1]) + 1]
+
     # Generate the histogram of the neighborhood
     hist_channel_0 = cv2.calcHist([image], [0], neighborhood, [256], [0, 256])
     hist_channel_1 = cv2.calcHist([image], [1],neighborhood, [256], [0, 256])
@@ -47,9 +45,9 @@ def crop_image(image, points):
     new_image = np.zeros_like(image)
 
     # Fill the new image with possible values based on the neighborhood distribution
-    for i in range(mask.shape[0]):
-        for j in range(mask.shape[1]):
-            if mask[i, j] == 255:
+    for i in range(dilated_mask.shape[0]):
+        for j in range(dilated_mask.shape[1]):
+            if dilated_mask[i, j] == 255:
                 # Get the intensity values from the random number
                 intensity_0 = np.argmax(cdf_channel_0 >= random_numbers[i, j])
                 intensity_1 = np.argmax(cdf_channel_1 >= random_numbers[i, j])
@@ -62,12 +60,10 @@ def crop_image(image, points):
 
     #Combine the new image with the original image to replace the object region
     
-    result = cv2.bitwise_and(image,image,mask= cv2.bitwise_not(mask))+new_image
+    result = cv2.bitwise_and(image,image,mask= cv2.bitwise_not(dilated_mask))+new_image
     result=cv2.medianBlur(result,3)
-    cv2.imshow('new_image',new_image)
-    cv2.imshow('resultado',result)
-
-    return mask,hist_channel_0,hist_channel_1,hist_channel_2
+    cv2.imshow('result',result)
+    return mask,hist_channel_0,hist_channel_1,hist_channel_2,new_image
 
 # Read video file
 video_path = "/home/enchi/Vídeos/2023_05_05_14_59_37-ball-detection.mp4"  # Replace with your video file path"
@@ -92,8 +88,9 @@ while cap.isOpened():
         break
 
     if crop_mode and len(clicked_points) >= 3:
-        mask,hist0,hist1,hist2=crop_image(frame, clicked_points)
-        #cv2.imshow(window_name, cropped_frame)
+        mask,hist0,hist1,hist2,new_image=crop_image(frame, clicked_points)
+        cv2.imshow('new_image',new_image)
+        cv2.imshow('mask',mask)
     else:
         # Draw clicked points
         for point in clicked_points:
@@ -121,11 +118,11 @@ while cap.isOpened():
             timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
             save_directory = os.path.abspath(os.path.join(os.getcwd(), "..", "img"))  # Replace "desired_directory" with your desired directory name
             os.makedirs(save_directory, exist_ok=True)
-            save_path = os.path.join(save_directory, "mask_{}.jpg".format(timestamp))
-            cv2.imwrite(save_path,mask)
-            print("Cropped image saved as:", save_path)
+            save_path = os.path.join(save_directory, "new_image_{}.jpg".format(timestamp))
+            cv2.imwrite(save_path,new_image)
+            print("New image saved as:", save_path)
         else:
-            print("Cannot save cropped image. Please enter crop mode and select at least 3 points.")
+            print("Cannot save New image. Please enter crop mode and select at least 3 points.")
    
 
     # Exit if 'q' is pressed
