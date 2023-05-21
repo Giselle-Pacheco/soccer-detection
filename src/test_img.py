568120579eb444172b52b8739d0d9ec4fadc549e
Juan Carlos <juan.sanchezp@udem.edu>
2023-05-20 18:04:22 -0600

    plot the  histogram of 3 channls of neighborhood

--- src/test_img.py
+++ src/test_img.py
@@ -13,39 +13,48 @@ def get_circle(event, x, y, flags, param):
     global clicked_points
     if event == cv2.EVENT_LBUTTONDOWN:
         clicked_points.append((x, y))
+
     
 
 def crop_image(image, points):
-    mask = np.zeros(image.shape[:2], dtype=np.uint8)
+    mask = np.zeros_like(image)
     points = np.array(points)
     cv2.fillPoly(mask, [points], (255, 255, 255))
     neighborhood_size = 5
     dilated_mask = cv2.dilate(mask, np.ones((neighborhood_size, neighborhood_size), np.uint8))
     neighborhood = cv2.absdiff(dilated_mask, mask)
     neighborhood_pixels = np.where(neighborhood > 0)
-
     neighborhood_region = image[min(neighborhood_pixels[0]):max(neighborhood_pixels[0]) + 1,
                             min(neighborhood_pixels[1]):max(neighborhood_pixels[1]) + 1]
-
+    
     # Generate the histogram of the neighborhood
-    histB = cv2.calcHist([neighborhood_region], [0], None, [256], [0, 256])
+    hist_channel_0 = cv2.calcHist([neighborhood_region], [0], None, [256], [0, 256])
+    hist_channel_1 = cv2.calcHist([neighborhood_region], [1], None, [256], [0, 256])
+    hist_channel_2 = cv2.calcHist([neighborhood_region], [2], None, [256], [0, 256])
 
-    # Plot the histogram
-    plt.plot(histB,color='b')
-    plt.title("Histogram of the Neighborhood")
-    plt.xlabel("Pixel Value")
-    plt.ylabel("Frequency")
-    plt.legend('B')
-    plt.show()
+    # Calculate the cumulative distribution function (CDF) for each channel
+    cdf_b = hist_channel_0.cumsum() / hist_channel_0.sum()
+    cdf_g = hist_channel_1.cumsum() / hist_channel_1.sum()
+    cdf_r = hist_channel_2.cumsum() / hist_channel_2.sum()
 
-    # Visualize the neighborhood
-    image[neighborhood_pixels] = [0, 0, 255]  # Mark neighborhood pixels as red
+    # Generate random values for each channel
+    rand_vals_b = np.random.rand(mask.shape[0], mask.shape[1])
+    rand_vals_g = np.random.rand(mask.shape[0], mask.shape[1])
+    rand_vals_r = np.random.rand(mask.shape[0], mask.shape[1])
 
-    cv2.imshow("Image with Neighborhood", image)
+    # Map the random values to pixel values using the CDFs
+    pixels_b = np.interp(rand_vals_b, cdf_b, np.arange(256))
+    pixels_g = np.interp(rand_vals_g, cdf_g, np.arange(256))
+    pixels_r = np.interp(rand_vals_r, cdf_r, np.arange(256))
 
-    #cv2.imshow('dilated_mask',neighborhood)
+
+    # Create the filled mask by replacing the masked pixels with the mapped values
+    filled_mask = mask.copy()
+    # Create an RGB array of the mapped pixel values
+    rgb_pixels = np.stack((pixels_b.astype(np.uint8), pixels_g.astype(np.uint8), pixels_r.astype(np.uint8)), axis=-1)
+
     cropped_image = cv2.bitwise_and(image, image, mask=mask)
-    return cropped_image,mask
+    return cropped_image,mask,hist_channel_0,hist_channel_1,hist_channel_2
 
 # Read video file
 video_path = "/home/enchi/Vídeos/2023_05_05_14_59_37-ball-detection.mp4"  # Replace with your video file path"
@@ -70,7 +79,7 @@ while cap.isOpened():
         break
 
     if crop_mode and len(clicked_points) >= 3:
-        cropped_frame,mask = crop_image(frame, clicked_points)
+        cropped_frame,mask,hist0,hist1,hist2= crop_image(frame, clicked_points)
         cv2.imshow(window_name, cropped_frame)
     else:
         # Draw clicked points
@@ -113,3 +122,15 @@ while cap.isOpened():
 # Release the video capture object and close all windows
 cap.release()
 cv2.destroyAllWindows()
+
+
+# Plot the histogram
+plt.figure(figsize=(8, 6))
+plt.plot(hist0, color='b', label='Blue')
+plt.plot(hist1, color='g', label='Green')
+plt.plot(hist2, color='r', label='Red')
+plt.title("Histograms of the Neighborhood (RGB)")
+plt.xlabel("Pixel Value")
+plt.ylabel("Frequency")
+plt.legend()
+plt.show()
