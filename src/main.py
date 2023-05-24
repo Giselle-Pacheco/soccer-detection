c5cf6166ab0ce19710c9107470fee2c25c94c18c
Juan Carlos <juan.sanchezp@udem.edu>
2023-05-24 15:46:01 -0600

    change the path

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 # ----------------------------------------------------------------
 # 
 # copy line to run:
-#    'python main.py --video_file  ../../2023_05_05_14_59_37-ball-detection.mp4'
-#    'python main.py --video_file  ../../2023_05_05_15_02_22-players-and-ball-detection.mp4'
+#    'python main.py --video_file  ../../../../Vídeos/2023_05_05_14_59_37-ball-detection.mp4'
+#    'python main.py --video_file  ../../../../Vídeos/2023_05_05_15_02_22-players-and-ball-detection.mp4'
 
 # ----------------------------------------------------------------
 
